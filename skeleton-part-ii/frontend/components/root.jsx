e44986d1073c933bb9627508222c9a1f927e9334
YWBK <ywbkdev@gmail.com>
2022-02-21 14:32:57 -0500

    finish phase 4

--- skeleton-part-ii/frontend/components/root.jsx
+++ skeleton-part-ii/frontend/components/root.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import PokemonIndexContainer from './../components/pokemon/pokemon_index_container';
 import { HashRouter } from "react-router-dom";
-
-
+import App from './app';
 
 const Root = ({store}) => (
   <Provider store={store}>
